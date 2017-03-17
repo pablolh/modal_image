dde7479d6763ddba99c5c746b6291834b9f6dbb4
Pablo Le Hénaff <pablo.le-henaff@polytechnique.edu>
2017-03-17 11:48:43 +0100

    travail du 10 mars

--- testwebcam.cpp
+++ testwebcam.cpp
@@ -1,51 +1,56 @@
- 
+#include <opencv2/opencv.hpp>
+
 #include <stdio.h>
-#include "highgui.h"
-#include "cv.h"
- 
-int main() {
- 
-    // Touche clavier
-    char key;
-    // Image
-     IplImage *image;
-    // Capture vidéo
-    CvCapture *capture;
- 
-    // Ouvrir le flux vidéo
-    //capture = cvCreateFileCapture("/path/to/your/video/test.avi"); // chemin pour un fichier
-    capture = cvCreateCameraCapture(CV_CAP_ANY);
- 
-    // Vérifier si l'ouverture du flux est ok
-    if (!capture) {
- 
-       printf("Ouverture du flux vidéo impossible !\n");
-       return 1;
- 
-    }
- 
-    // Définition de la fenêtre
-    cvNamedWindow("GeckoGeek Window", CV_WINDOW_AUTOSIZE);
- 
-    // Boucle tant que l'utilisateur n'appuie pas sur la touche q (ou Q)
-    while(key != 'q' && key != 'Q') {
- 
-       // On récupère une image
-       image = cvQueryFrame(capture);
-		
-		
-		
-       // On affiche l'image dans une fenêtre
-       cvShowImage( "GeckoGeek Window", image);
- 
-       // On attend 10ms
-       key = cvWaitKey(10);
- 
-    }
- 
-    cvReleaseCapture(&capture);
-    cvDestroyWindow("GeckoGeek Window");
- 
-    return 0;
- 
+#include <unistd.h>
+using namespace cv;
+using namespace std;
+
+
+int t = 180;
+int t2 = 100;
+int p = 10;
+int h = 400;
+Ptr<BackgroundSubtractorMOG2> bs;
+
+static void trackmask( int, void* )
+{
+	bs->setVarThreshold(t2/5);
+}
+static void trackhistory( int, void* )
+{
+	bs->setHistory(h);
+}
+
+int main(int, char**) {
+	Mat frame;
+	Mat mask;
+	Mat frame2;
+	VideoCapture cap;
+	cap.open(0);
+	
+	namedWindow("Mask", WINDOW_AUTOSIZE);
+	namedWindow("Contour", WINDOW_AUTOSIZE);
+	bs = createBackgroundSubtractorMOG2();
+	bs->setHistory(h);
+	bs->setNMixtures(3);
+	bs->setDetectShadows(false);
+	bs->setVarThreshold(20);
+	//~ bs->setComplexityReductionThreshold(0);
+	//~ bs->setBackgroundRatio(1);
+	Mat frame0;
+
+	cap.read(frame0);
+	while(true) {
+		cap.read(frame);
+		imshow("webcam", frame);
+		bs->apply(frame, mask);
+		morphologyEx(mask, mask, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(4, 4)));
+		morphologyEx(mask, mask, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, Size(2, 2)));
+		// bs->getBackgroundImage(frame2);
+		Canny(mask, frame2, t, t*3);
+		imshow("Mask", mask);
+		imshow("Contour", frame2);
+		waitKey(30);
+	}
+	
 }
